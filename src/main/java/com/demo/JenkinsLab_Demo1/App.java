4b0e29a4b04393ca3d21414743357b9e6ec0d471
moidul026 <moidul.islam75@gmail.com>
2023-12-04 22:53:49 +0530

    adding project 1

---
+++ src/main/java/com/demo/JenkinsLab_Demo1/App.java
@@ -0,0 +1,13 @@
+package com.demo.JenkinsLab_Demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
